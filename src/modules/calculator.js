01d1ca5ac450bb634cb7c5d3d4ffcbb8f33fb396
GuzeevZhenya <shyst.evgeny@mail.ru>
2021-05-17 11:46:59 +0300

    Пофиксил ошибки с калькулятором

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -1,12 +1,12 @@
 const calculator = () => {
 	let priceTotal = document.querySelector('#price-total');
-	const calculatorBlock = document.querySelector('.time');
+	const calculatorBlock = document.querySelector('#card_order');
 	const inputPromocode = document.querySelector('#input-promocode');
 
-	const pricesClubs = {
+	const clubsPrices = {
 		//массивы с ценами для разных клубов
 		mozaika: [1999, 9900, 13900, 19900],
-		schelkovo: [2999, 14990, 21990,14990, 24990],
+		schelkovo: [2999, 14990, 21990, 24990],
 	};
 
 	let selectedClub = 'mozaika',
@@ -14,27 +14,27 @@ const calculator = () => {
 		promocode = 1;
 
 	//Вывод данных
-	const outputPriceTotal = (club, month) => (priceTotal.textContent = Math.floor(pricesClubs[club][month] * promocode));
+	const outputPriceTotal = (club, month) => (priceTotal.textContent = Math.floor(clubsPrices[club][month] * promocode));
 
 	if (calculatorBlock) {
 		outputPriceTotal(selectedClub, selectedMonth);
-
 		calculatorBlock.addEventListener('change', (event) => {
 			//слушатель на изменения в калькуляторе
 			const target = event.target;
 			if (target.getAttribute('name') === 'club-name') {
-				selectedClub = target.value; //какой клуб выбираем, то значение инпута и записывается в переменную
-			}
+        selectedClub = target.value; //какой клуб выбираем, то значение инпута и записывается в переменную
+      }
 			if (target.getAttribute('name') === 'card-type') {
 				selectedMonth = target.value; //в переменную записывается соответстующее значение выбранному кол-ву месяцев
 			}
+			 
 			outputPriceTotal(selectedClub, selectedMonth); //рассчет и вывод итоговой цены
 		});
 
 		inputPromocode.addEventListener('input', () => {
-			console.log(inputPromocode.value)
+
 			//при введении промокода "ТЕЛО2020" цена уменьшается на 30%
-			if (inputPromocode.value === 'ТЕЛО2020') {
+			if ((inputPromocode.value).toUpperCase() === 'ТЕЛО2019') {
 				promocode = 0.7;
 			} else {
 				promocode = 1;
@@ -43,6 +43,5 @@ const calculator = () => {
 		});
 	}
 }
-
 
 export default calculator;
